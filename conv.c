c7afbba4784a48bee7f056499249dd7517ce64fa
Gustavo Gallo <gustavotibollagallo@gmail.com>
2024-12-05 10:11:59 -0300

    variação entrada e saída

--- conv.c
+++ conv.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void invertvet(int tam, int vet[])
 {
@@ -36,24 +37,47 @@ void convolution(int *sinal, int tamSinal, int *filtro, int tamFiltro, int *said
     }
 }
 
+int lerArquivo(const char *nomeArquivo, int **vetor)
+{
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (!arquivo)
+    {
+        printf("Erro ao abrir o arquivo: %s\n", nomeArquivo);
+        exit(1);
+    }
+
+    int tamanho;
+    fscanf(arquivo, "%d", &tamanho); // Primeiro número é o tamanho do vetor
+
+    *vetor = malloc(tamanho * sizeof(int));
+    for (int i = 0; i < tamanho; i++)
+    {
+        fscanf(arquivo, "%d", &(*vetor)[i]);
+    }
+
+    fclose(arquivo);
+    return tamanho;
+}
+
 int main()
 {
-    int sinal[] = {1, 2, 3, 4, 5};
-    int filtro[] = {-1, 0, 1};
-    int tamFiltro = sizeof(filtro) / sizeof(filtro[0]);
-    int tamSinal = sizeof(sinal) / sizeof(sinal[0]);
+    int *sinal, *filtro;
+    int tamFiltro = lerArquivo("filtro.txt", &filtro);
+    int tamSinal = lerArquivo("sinal.txt", &sinal);
 
     int tamanhoSaida = tamSinal + tamFiltro - 1;
     int saida[tamanhoSaida];
 
-    convolution(sinal,tamSinal,filtro,tamFiltro,saida);
-
+    convolution(sinal, tamSinal, filtro, tamFiltro, saida);
 
     for (int i = 0; i < tamanhoSaida; i++)
     {
         printf("%d ", saida[i]);
     }
     printf("\n");
+
+    free(sinal);
+    free(filtro);
 
     return 0;
 }
