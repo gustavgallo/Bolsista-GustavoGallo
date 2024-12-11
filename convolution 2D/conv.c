82fbb54695db8f2b68f06a1595cfaaf2c4bbf772
Gustavo Gallo <gustavotibollagallo@gmail.com>
2024-12-11 13:33:31 -0300

    conv 2D

--- convolution 2D/conv.c
+++ convolution 2D/conv.c
@@ -1,59 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define TAM_ENTRADA 3
 #define TAM_KERNEL 2
+#define TAM_SAIDA (TAM_ENTRADA - TAM_KERNEL + 1)
 
+void convolucao2D(int entrada[][TAM_ENTRADA], int kernel[][TAM_KERNEL], int tamEntrada, int tamKernel, int saida[][TAM_SAIDA]);
 
 int main()
 {
-   
-int entrada[TAM_ENTRADA][TAM_ENTRADA] = {
-    {1,2,3},
-    {4,5,6},
-    {7,8,9}
-};
+    int entrada[TAM_ENTRADA][TAM_ENTRADA] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}};
 
-int kernel[TAM_KERNEL][TAM_KERNEL] ={
-    {1, 0},
-    {0,-1},
-};
+    int kernel[TAM_KERNEL][TAM_KERNEL] = {
+        {1, 0},
+        {0, -1}};
 
-int tamSaida = TAM_ENTRADA - TAM_KERNEL + 1;
-int saida[tamSaida][tamSaida];
+    int saida[TAM_SAIDA][TAM_SAIDA] = {0};
 
-  for (int i = 0; i < tamSaida; i++) {
-        for (int j = 0; j < tamSaida; j++) {
-            saida[i][j] = 0;
-        }
-    }
+    convolucao2D(entrada, kernel, TAM_ENTRADA, TAM_KERNEL, saida);
 
-       // Exibir as matrizes para verificar
-    printf("Matriz de Entrada:\n");
-    for (int i = 0; i < TAM_ENTRADA; i++) {
-        for (int j = 0; j < TAM_ENTRADA; j++) {
-            printf("%d ", entrada[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\nFiltro:\n");
-    for (int i = 0; i < TAM_KERNEL; i++) {
-        for (int j = 0; j < TAM_KERNEL; j++) {
-            printf("%d ", kernel[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\nMatriz de Saida (inicializada com zeros):\n");
-    for (int i = 0; i < tamSaida; i++) {
-        for (int j = 0; j < tamSaida; j++) {
+    printf("Matriz de saída:\n");
+    for (int i = 0; i < TAM_SAIDA; i++)
+    {
+        for (int j = 0; j < TAM_SAIDA; j++)
+        {
             printf("%d ", saida[i][j]);
         }
         printf("\n");
     }
 
+    return 0;
+}
 
-
-
-} 
+// não é necessário colocar o tamanho das linhas, apenas colunas, por conta do compilador
+void convolucao2D(int entrada[][TAM_ENTRADA], int kernel[][TAM_KERNEL], int tamEntrada, int tamKernel, int saida[][TAM_SAIDA])
+{
+    for (int i = 0; i < TAM_SAIDA; i++)
+    {
+        for (int j = 0; j < TAM_SAIDA; j++)
+        {
+            int soma = 0;
+            for (int k = 0; k < tamKernel; k++)
+            {
+                for (int l = 0; l < tamKernel; l++)
+                {
+                    soma += kernel[k][l] * entrada[i + k][j + l];// o que altera é a região que o filtro está "observando"
+                }
+            }
+            saida[i][j] = soma;
+        }
+    }
+}
