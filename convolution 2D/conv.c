e2c4d6a19ca07d8697dc38c323b8eeb1eb4f0d71
Gustavo Gallo <gustavotibollagallo@gmail.com>
2024-12-17 19:08:51 -0300

    Realizei a convolução 2D usando vetores unidimensionais

--- convolution 2D/conv.c
+++ convolution 2D/conv.c
@@ -4,7 +4,7 @@
 #define TAM_KERNEL 2
 #define TAM_SAIDA (TAM_ENTRADA - TAM_KERNEL + 1)
 
-void convolucao2D(int entrada[][TAM_ENTRADA], int kernel[][TAM_KERNEL], int tamEntrada, int tamKernel, int saida[][TAM_SAIDA]);
+void convolucao2D(int entrada1D[], int kernel1D[], int tamEntrada, int tamKernel, int saida[]);
 
 int main()
 {
@@ -17,16 +17,38 @@ int main()
         {1, 0},
         {0, -1}};
 
-    int saida[TAM_SAIDA][TAM_SAIDA] = {0};
+//criei as versóes 1D das matrizes 
+    int entrada1D[TAM_ENTRADA * TAM_ENTRADA];
+    int kernel1D[TAM_KERNEL * TAM_KERNEL];
 
-    convolucao2D(entrada, kernel, TAM_ENTRADA, TAM_KERNEL, saida);
+// transferi a 2D da entrada para 1D
+    for(int i = 0; i < TAM_ENTRADA; i++){
+        for(int j = 0 ; j < TAM_ENTRADA; j++){
+            entrada1D[i * TAM_ENTRADA + j] = entrada[i][j];
+
+        }
+    }
+// transferi a 2D do kernel para 1D
+    for(int i = 0; i < TAM_KERNEL; i++){
+        for(int j = 0 ; j < TAM_KERNEL; j++){
+            kernel1D[i * TAM_KERNEL + j] = kernel[i][j];
+
+        }
+    }
+    
+
+    int saida[TAM_SAIDA * TAM_SAIDA] = {0};
+
+
+
+    convolucao2D(entrada1D, kernel1D, TAM_ENTRADA, TAM_KERNEL, saida);
 
     printf("Matriz de saída:\n");
     for (int i = 0; i < TAM_SAIDA; i++)
     {
         for (int j = 0; j < TAM_SAIDA; j++)
         {
-            printf("%d ", saida[i][j]);
+            printf("%d ", saida[i * TAM_SAIDA + j]);
         }
         printf("\n");
     }
@@ -34,22 +56,22 @@ int main()
     return 0;
 }
 
-// não é necessário colocar o tamanho das linhas, apenas colunas, por conta do compilador
-void convolucao2D(int entrada[][TAM_ENTRADA], int kernel[][TAM_KERNEL], int tamEntrada, int tamKernel, int saida[][TAM_SAIDA])
+//  feita convolução 2D, utilizando vetores unidimensionais
+void convolucao2D(int entrada1D[], int kernel1D[], int colEntrada, int colKernel, int saida[])
 {
     for (int i = 0; i < TAM_SAIDA; i++)
     {
         for (int j = 0; j < TAM_SAIDA; j++)
         {
             int soma = 0;
-            for (int k = 0; k < tamKernel; k++)
+            for (int l = 0; l < TAM_KERNEL; l++)
             {
-                for (int l = 0; l < tamKernel; l++)
+                for (int c = 0; c < TAM_KERNEL; c++)
                 {
-                    soma += kernel[k][l] * entrada[i + k][j + l];// o que altera é a região que o filtro está "observando"
+                    soma += kernel1D[l * colKernel + c] * entrada1D[(i + l) * colEntrada + (j + c)];
                 }
             }
-            saida[i][j] = soma;
+            saida[i * TAM_SAIDA + j] = soma;
         }
     }
 }
